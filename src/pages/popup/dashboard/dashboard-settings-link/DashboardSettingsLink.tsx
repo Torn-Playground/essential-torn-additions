2d8938e4c94ca394ba8443d7973307dbf9f2ca38
DeKleineKobini <kobedepeuter@outlook.com>
2022-07-10 10:17:17 +0200

    .

--- src/pages/popup/dashboard/dashboard-settings-link/DashboardSettingsLink.tsx
+++ src/pages/popup/dashboard/dashboard-settings-link/DashboardSettingsLink.tsx
@@ -1,8 +1,22 @@
-import { browser } from "webextension-polyfill-ts";
+import { browser, Manifest } from "webextension-polyfill-ts";
+import { useEffect, useState } from "react";
 
 export default function DashboardSettingsLink() {
+    const [url, setUrl] = useState<string>();
+
+    useEffect(() => {
+        const manifest = browser.runtime.getManifest() as Manifest.WebExtensionManifest;
+        if (!manifest.options_ui) {
+            return;
+        }
+
+        const url = browser.runtime.getURL(manifest.options_ui.page);
+
+        setUrl(url);
+    }, []);
+
     return (
-        <a href={browser.runtime.getURL("pages/settings/settings.html")} target="_blank">
+        <a href={url} target="_blank">
             Settings
         </a>
     );
