b4d558ca68bed8e48848b9d7b69206cdeee6ee78
DeKleineKobini <kobedepeuter@outlook.com>
2022-07-11 21:14:53 +0200

    Remove chain bar.

--- src/pages/popup/dashboard/Dashboard.tsx
+++ src/pages/popup/dashboard/Dashboard.tsx
@@ -10,7 +10,6 @@ import DashboardUpdated from "./dashboard-updated/DashboardUpdated";
 import DashboardTravelBar from "@pages/popup/dashboard/dashboard-bar/DashboardTravelBar";
 import DashboardIndicators from "@pages/popup/dashboard/dashboard-indicators/DashboardIndicators";
 import DashboardStatus from "@pages/popup/dashboard/dashboard-status/DashboardStatus";
-import DashboardChainBar from "@pages/popup/dashboard/dashboard-bar/DashboardChainBar";
 import DashboardSettingsLink from "@pages/popup/dashboard/dashboard-settings-link/DashboardSettingsLink";
 
 export default function Dashboard() {
@@ -45,16 +44,17 @@ export default function Dashboard() {
                 <DashboardBar name="Nerve" bar={data.nerve} link="https://www.torn.com/crimes.php" progressColor="#cc4b2d" />
                 <DashboardBar name="Happy" bar={data.happy} link="https://www.torn.com/properties.php" progressColor="#ccb62a" resetWhenOver={true} />
                 <DashboardBar name="Life" bar={data.life} link="https://www.torn.com/item.php#medical-items" progressColor="#3f43cf" />
-                {data.chain.current > 0 ? (
-                    <DashboardChainBar
-                        name="Chain"
-                        currentValue={data.chain.current}
-                        maximumValue={data.chain.maximum}
-                        timeout={data.chain.timeout}
-                        link="https://www.torn.com/factions.php?step=your"
-                        progressColor="#6b6b6b"
-                    />
-                ) : undefined}
+                {/* TODO - Reimplement*/}
+                {/*{data.chain.current > 0 ? (*/}
+                {/*    <DashboardChainBar*/}
+                {/*        name="Chain"*/}
+                {/*        currentValue={data.chain.current}*/}
+                {/*        maximumValue={data.chain.maximum}*/}
+                {/*        timeout={data.chain.timeout}*/}
+                {/*        link="https://www.torn.com/factions.php?step=your"*/}
+                {/*        progressColor="#6b6b6b"*/}
+                {/*    />*/}
+                {/*) : undefined}*/}
                 {typeof data.travel.timeLeft !== "undefined" ? (
                     <DashboardTravelBar
                         name="Travelling"
